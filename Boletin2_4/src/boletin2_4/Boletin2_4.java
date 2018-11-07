d83e79324b37cd1df9fb6b9f2ca3dce7864b8ce9
slorenzorodriguez <slorenzorodriguez@nautilus22.danielcastelao.org>
2018-11-07 12:09:38 +0100

    ramas netbeans

--- Boletin2_4/src/boletin2_4/Boletin2_4.java
+++ Boletin2_4/src/boletin2_4/Boletin2_4.java
@@ -30,7 +30,22 @@ public class Boletin2_4 {
         System.out.println(cantidad1+" euros son "+billetesDe20+ "billetes de 20 euros");
         System.out.println(cantidad1+" euros son "+billetesDe5+ "billetes de 5 euros");
         System.out.println(cantidad1+" euros son "+moedasDe1+ "moedas de 1 euro");
-                
+        
+        
+        // Ejercicio 5, Boletin 2:
+        Scanner leer = new Scanner (System.in);
+         double sueldoBruto, sueldoLiquido , sueldoFijo, km, dias, ventas;
+        System.out.println("Introduzca sueldo fijo: ");
+        sueldoFijo = leer.nextDouble();
+        System.out.println("Introduzca quilometraje: ");
+        km = leer.nextDouble();
+        System.out.println("Introduzca dias desplazamiento: ");
+        dias = leer.nextDouble();
+        System.out.println("Importe de las ventas ");
+        ventas = leer.nextDouble();
+        sueldoBruto= sueldoFijo + (km*2) + (dias*30) + (ventas*5/100);
+        sueldoLiquido= sueldoBruto - (sueldoBruto*18/100) - 36;
+        System.out.println("El empleado tiene un saldo bruto de "+sueldoBruto+" € pero su saldo neto es de " +sueldoLiquido+ "€");        
     }
     
 }
